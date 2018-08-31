d961d70a2d7970582e53cc6b99fd92d87a9830a6
Jim Dibb <jim.dibb@emc.com>
2018-08-30 23:08:36 -0400

    add alt-mouse layer for easy alt  tab

--- layouts/community/ergodox/dvorak_jimd/keymap.c
+++ layouts/community/ergodox/dvorak_jimd/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,                    KC_F1,          KC_F2,       KC_F3,        KC_F4,       KC_F5,       MEH_T(KC_F6),
         KC_TAB,                    KC_QUOT,        KC_COMM,     KC_DOT,       KC_P,        KC_Y,        MO(KEY_SEL),
         MO(BRACKETS),              KC_A,           KC_O,        KC_E,         KC_U,        KC_I,
-        MO(SHELL_NAV),             KC_SCLN,        KC_Q,        KC_J,         KC_K,        KC_X,        MO(KEY_NAV),
+        LM(MOUSE, MOD_LALT),             KC_SCLN,        KC_Q,        KC_J,         KC_K,        KC_X,        MO(KEY_NAV),
                    MO(MOUSE),OSM(MOD_LCTL), OSM(MOD_LALT),OSL(SYMBOL),MO(NUMBER),  
                                        // thumb cluster
                                                SFT_T(KC_PAUSE), LCTL(KC_S),
@@ -254,7 +254,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_MS_WH_LEFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_MS_WH_RIGHT, KC_MS_ACCEL1,
        M(VS_EMAIL),  KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_ACCEL2,
                           KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS,
+       LALT(LSFT(KC_TAB)), LALT(KC_TAB),
        KC_TRNS,
 	   KC_TRNS, KC_BTN1, KC_BTN2
 ),
