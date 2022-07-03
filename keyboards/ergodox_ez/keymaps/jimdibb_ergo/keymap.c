c83cdf05d9e4639a4d46e983e04d85d09251e8df
Jim Dibb <jimdibb@gmail.com>
2022-07-02 22:03:14 -0400

    adapted macros to new use case

--- keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
+++ keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
@@ -1,49 +1,54 @@
-
 #include QMK_KEYBOARD_H
 #include "led.h"
-#include "debug.h"
+#include "version.h"
 #include "action_layer.h"
 #include "action_code.h"
+#include "mousekey.h"
 
-#define BASE    0 // default layer
-#define SHELL_NAV 1
-#define KEY_NAV 2 // key navigation layer
-#define KEY_SEL 3 // key selection layer
-#define NUMBER  4  // number layer
-#define SYMBOL  5
-#define BRACKETS 6
-#define SHORTCUTS 7
-#define MOUSE 8
-#define RSMITH 9
-#define GAME 10
+enum keyboard_layers {
+    BASE = 0,
+    //SHELL_NAV,
+    KEY_NAV,
+    KEY_SEL,
+    NUMBER,
+    SYMBOL,
+    BRACKETS,
+    SHORTCUTS,
+    MOUSE,
+    RSMITH,
+    GAME,
+};
+
 
 // macros
-#define MC_COPY_LINE  0
-#define MC_CUT_LINE   1
-#define MC_PASTE_LINE 2
-#define MC_NEW_SEARCH_TAB 3
-#define SCREEN_TAB_LEFT 4
-#define SCREEN_TAB_RIGHT 5
-#define SCREEN_NEW_TAB 6
-#define SWITCH_NDS 7
-#define SCREEN_COPY_MODE 8
-#define SCREEN_PASTE 9
-#define OPEN_CLOSE_PAREN 10
-#define OPEN_CLOSE_BRACKET 11
-#define OPEN_CLOSE_CURLY 12
-#define OPEN_CLOSE_SINGLE_QUOTE 13
-#define OPEN_CLOSE_DOUBLE_QUOTE 14
-#define SHELL_RECALL_LAST_ARG_REMOVE_FIRST_COMMAND 15
-#define SEMICOLON_NEWLINE 16
-#define END_NEWLINE 17
-#define GMAIL 18
-#define VS_EMAIL 19
-#define RS_AS 20
-#define GAME_AS 21
+enum keyboard_macros {
+    // ! commented values not used
+    // MC_NEW_SEARCH_TAB,
+    // SCREEN_TAB_LEFT,
+    // SCREEN_TAB_RIGHT,
+    // SCREEN_NEW_TAB,
+    // SWITCH_NDS,
+    // SCREEN_COPY_MODE,
+    // SCREEN_PASTE,
+    OCPAREN = SAFE_RANGE,
+    OCBRAC,
+    OCCURL,
+    OCSINGLE,
+    OCDOUBLE,
+    SHELL_RECALL_LAST_ARG_REMOVE_FIRST_COMMAND,
+    SEMICOLON_NEWLINE,
+    END_NEWLINE,
+    GMAIL,
+    VS_EMAIL,
+    DELL,
+    GAME_AS,
+    MCPYLINE,
+    MCUTLINE,
+    MPASTELINE,
+    SWITCH_NDS,
+    RS_AS,
 
-// pause opens terminal in code
-
-
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // base layer
@@ -72,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 // shell navigation layer
-[SHELL_NAV] = LAYOUT_ergodox(
+/* [SHELL_NAV] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -96,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
-
+ */
 
 // key navigation layer
 [KEY_NAV] = LAYOUT_ergodox(
@@ -113,9 +118,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        RCTL(KC_1), RCTL(KC_2),        RCTL(KC_3),    RCTL(RSFT(KC_PGUP)),    RCTL(RSFT(KC_PGDN)),    KC_TRNS,        KC_TRNS,
-       KC_TRNS, KC_PGDN,        KC_HOME,    KC_UP,      KC_END,     KC_PGUP,        M(MC_COPY_LINE),
-                RCTL(KC_LEFT),  KC_LEFT,    KC_DOWN,    KC_RIGHT,   RCTL(KC_RIGHT), M(MC_CUT_LINE),
-       KC_TRNS, KC_TRNS,        RCTL(KC_C), RCTL(KC_X), RCTL(KC_V), RCTL(KC_Z),        M(MC_PASTE_LINE),
+       KC_TRNS, KC_PGDN,        KC_HOME,    KC_UP,      KC_END,     KC_PGUP,        MCPYLINE,
+                RCTL(KC_LEFT),  KC_LEFT,    KC_DOWN,    KC_RIGHT,   RCTL(KC_RIGHT), MCUTLINE,
+       KC_TRNS, KC_TRNS,        RCTL(KC_C), RCTL(KC_X), RCTL(KC_V), RCTL(KC_Z),     MPASTELINE,
                 // bottom row
                 KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,
        // thumb cluster
@@ -139,9 +144,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS,       KC_TRNS,             KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS,              KC_TRNS,
-       RSFT(RCTL(KC_HOME)), RSFT(KC_PGDN),       RSFT(KC_HOME), RSFT(KC_UP),   RSFT(KC_END),   RSFT(KC_PGUP),        M(MC_COPY_LINE),
-                      RSFT(RCTL(KC_LEFT)), RSFT(KC_LEFT), RSFT(KC_DOWN), RSFT(KC_RIGHT), RSFT(RCTL(KC_RIGHT)), M(MC_CUT_LINE),
-       RSFT(RCTL(KC_END)), RCTL(KC_A),             RCTL(KC_C),    RCTL(KC_X),    RCTL(KC_V),     RCTL(KC_Z),              M(MC_PASTE_LINE),
+       RSFT(RCTL(KC_HOME)), RSFT(KC_PGDN),       RSFT(KC_HOME), RSFT(KC_UP),   RSFT(KC_END),   RSFT(KC_PGUP),  MCPYLINE,
+                      RSFT(RCTL(KC_LEFT)), RSFT(KC_LEFT), RSFT(KC_DOWN), RSFT(KC_RIGHT), RSFT(RCTL(KC_RIGHT)), MCUTLINE,
+       RSFT(RCTL(KC_END)), RCTL(KC_A),             RCTL(KC_C),    RCTL(KC_X),    RCTL(KC_V),     RCTL(KC_Z),   MPASTELINE,
                 // bottom row
                 KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,
        // thumb cluster
@@ -193,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_MINS, KC_DLR,  KC_PERC, KC_CIRC, KC_PIPE, KC_TRNS,
        KC_TRNS, KC_EQUAL,KC_EXLM, KC_AT,   KC_HASH, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       M(SEMICOLON_NEWLINE), M(END_NEWLINE),
+       SEMICOLON_NEWLINE, END_NEWLINE,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
 ),
@@ -201,7 +206,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BRACKETS] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,KC_TRNS,KC_TRNS,             KC_TRNS,            KC_TRNS,               KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,M(OPEN_CLOSE_CURLY), M(OPEN_CLOSE_PAREN),M(OPEN_CLOSE_BRACKET), KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,OCCURL,              OCPAREN,            OCBRAC,                 KC_TRNS,KC_TRNS,
        KC_TRNS,KC_LPRN, KC_RPRN,            KC_LBRC,            KC_RBRC,               KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,             KC_LCBR,            KC_RCBR,               KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,             KC_TRNS,            KC_TRNS,
@@ -210,7 +215,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS, KC_TRNS, M(OPEN_CLOSE_BRACKET),M(OPEN_CLOSE_PAREN),M(OPEN_CLOSE_CURLY),KC_TRNS,KC_TRNS,
+       KC_TRNS, KC_TRNS, OCBRAC,OCPAREN,OCCURL,KC_TRNS,KC_TRNS,
                 KC_TRNS, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_TRNS,
        KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -230,7 +235,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                    MEH(KC_2),
                                MEH(KC_3),MEH(KC_4),MEH(KC_5),
        // right hand
-       MEH(KC_F7), MEH(KC_F8), MEH(KC_F9), MEH(KC_F10),  MEH(KC_F11), MEH(KC_F12),  M(SWITCH_NDS),
+       MEH(KC_F7), MEH(KC_F8), MEH(KC_F9), MEH(KC_F10),  MEH(KC_F11), MEH(KC_F12),  SWITCH_NDS,
        KC_TRNS, MEH(KC_A), MEH(KC_B),    MEH(KC_C),    MEH(KC_D),    MEH(KC_E), MEH(KC_F),
                 MEH(KC_G), MEH(KC_H),    MEH(KC_I),    MEH(KC_J),    MEH(KC_K), MEH(KC_L),
        KC_TRNS, MEH(KC_M), MEH(KC_N),    MEH(KC_O),    MEH(KC_P),    MEH(KC_Q), KC_CAPSLOCK,
@@ -249,13 +254,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,KC_ASDN, KC_ASUP, KC_ASRP,  KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  M(RS_AS),KC_TRNS, KC_TRNS,
+                                  RS_AS,KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LWIN,
-       M(GMAIL),  KC_TRNS, KC_MS_WH_UP, KC_MS_U, KC_TRNS, KC_TRNS, KC_MS_ACCEL0,
+       DELL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LWIN,
+       GMAIL,  KC_TRNS, KC_MS_WH_UP, KC_MS_U, KC_TRNS, KC_TRNS, KC_MS_ACCEL0,
                  KC_MS_WH_LEFT, KC_MS_L, KC_MS_D, KC_MS_R, KC_MS_WH_RIGHT, KC_MS_ACCEL1,
-       M(VS_EMAIL),  KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_ACCEL2,
-                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, M(GAME_AS),
+       VS_EMAIL,  KC_TRNS, KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_ACCEL2,
+                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, GAME_AS,
        LALT(LSFT(KC_TAB)), LALT(KC_TAB),
        KC_TRNS,
 	   KC_TRNS, KC_BTN1, KC_BTN2
@@ -271,7 +276,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                        // thumb cluster
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
-                               M(RS_AS),KC_TRNS,KC_TRNS,
+                               RS_AS,KC_TRNS,KC_TRNS,
        // right hand
        KC_NO, KC_NO,        KC_NO,    KC_NO,    KC_9,    KC_0,        KC_SPACE,
        KC_LBRC, KC_PGUP,        KC_HOME,    KC_UP,      KC_O,     KC_P,        KC_TAB,
@@ -302,7 +307,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_H,  KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
        KC_RBRC, KC_N,        KC_M,    KC_COMM,    KC_DOT, KC_SLSH,        KC_RSHIFT,
                 // bottom row
-                KC_NO, KC_LSHIFT, KC_KP_MINUS,  KC_KP_PLUS,   M(GAME_AS),
+                KC_NO, KC_LSHIFT, KC_KP_MINUS,  KC_KP_PLUS,   GAME_AS,
        // thumb cluster
        KC_LEFT, KC_RIGHT,
        KC_UP,
@@ -310,133 +315,119 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 )
 };
 
-
-
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case MC_COPY_LINE:
-            if (record->event.pressed) {
-                return MACRO( T(HOME), D(LSFT), T(END), U(LSFT), D(LCTL), T(C), U(LCTL), END);
-            }
-            break;
-        case MC_CUT_LINE:
-            if (record->event.pressed) {
-                return MACRO( T(HOME), D(LSFT), T(END), U(LSFT), D(LCTL), T(X), U(LCTL), END);
-            }
-            break;
-        case MC_PASTE_LINE:
-            if (record->event.pressed) {
-                return MACRO( T(END), T(ENTER), D(LCTL), T(V), U(LCTL), END);
-            }
-            break;
-        case MC_NEW_SEARCH_TAB:
-             if (record->event.pressed) {
-                return MACRO( D(LCTL), T(T), T(K), U(LCTL), END);
-            }
-            break;
-        case SCREEN_TAB_LEFT:
-             if (record->event.pressed) {
-                return MACRO( D(LCTL), T(A), U(LCTL), T(P), END);
-            }
-            break;
-        case SCREEN_TAB_RIGHT:
-             if (record->event.pressed) {
-                return MACRO( D(LCTL), T(A), U(LCTL), T(N), END);
-            }
-            break;
-        case SCREEN_NEW_TAB:
-             if (record->event.pressed) {
-                return MACRO( D(LCTL), T(A), U(LCTL), T(C), END);
-            }
-        break;
-        case SCREEN_COPY_MODE:
-            if (record->event.pressed) {
-                return MACRO( D(LCTL), T(A), U(LCTL), T(ESC), END);
-            }
-        break;
-        case SCREEN_PASTE:
-            if (record->event.pressed) {
-                return MACRO( D(LCTL), T(A), U(LCTL), T(RBRC), END);
-            }
-        break;
-        case SWITCH_NDS:
-             if (record->event.pressed) {
-                return MACRO( D(LSFT), T(F11), U(LSFT), W(255), D(LALT), T(TAB), U(LALT), END);
-            }
-        break;
-        case OPEN_CLOSE_PAREN:
-            if (record->event.pressed) {
-                return MACRO( D(LSFT), T(9), T(0), U(LSFT), T(LEFT), END);
-            }
-        break;
-        case OPEN_CLOSE_BRACKET:
-            if (record->event.pressed) {
-                return MACRO( T(LBRC), T(RBRC), T(LEFT), END);
-            }
-        break;
-        case OPEN_CLOSE_CURLY:
-            if (record->event.pressed) {
-                return MACRO( D(LSFT), T(LBRC), T(RBRC), U(LSFT), T(LEFT), END);
-            }
-        break;
-        case OPEN_CLOSE_SINGLE_QUOTE:
-            if (record->event.pressed) {
-                return MACRO( T(QUOT), T(QUOT), T(LEFT), END);
-            }
-        break;
-        case OPEN_CLOSE_DOUBLE_QUOTE:
-            if (record->event.pressed) {
-                return MACRO( D(LSFT), T(QUOT), T(QUOT), U(LSFT), T(LEFT), END);
-            }
-        break;
-        case SHELL_RECALL_LAST_ARG_REMOVE_FIRST_COMMAND:
-            if (record->event.pressed) {
-                return MACRO( T(UP), T(HOME), D(LALT), T(D), U(LALT), END);
-            }
-        break;
-		case SEMICOLON_NEWLINE:
-            if (record->event.pressed) {
-                return MACRO( T(END), T(SCLN), T(ENTER), END);
-            }
-		break;
-		case END_NEWLINE:
-            if (record->event.pressed) {
-                return MACRO( T(END), T(ENTER), END);
-            }
-		break;
-		case GMAIL:
-            if (record->event.pressed) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed){
+        switch (keycode) {
+            case OCPAREN:
+            // D(LSFT), T(9), T(0), U(LSFT), T(LEFT), END);
+                SEND_STRING(SS_LSFT("90")SS_TAP(X_LEFT));
+                return false;
+            case OCBRAC:
+            // D(LSFT), T(9), T(0), U(LSFT), T(LEFT), END);
+                SEND_STRING("[]"SS_TAP(X_LEFT));
+                return false;
+            case OCCURL:
+            // D(LSFT), T(9), T(0), U(LSFT), T(LEFT), END);
+                SEND_STRING(SS_LSFT("[]")SS_TAP(X_LEFT));
+                return false;
+            case OCSINGLE:
+                SEND_STRING("''" SS_TAP(X_LEFT));
+                return false;
+            case OCDOUBLE:
+                SEND_STRING(SS_DOWN(X_LSFT) "''"  SS_UP(X_LSFT) SS_TAP(X_LEFT));
+                return false;
+            case MCPYLINE:
+            // T(HOME), D(LSFT), T(END), U(LSFT), D(LCTL), T(C), U(LCTL), END);
+                SEND_STRING(SS_TAP(X_HOME) SS_DOWN(X_LSFT) SS_TAP(X_END) SS_UP(X_LSFT) SS_LCTL("c"));
+                return false;
+            case MCUTLINE:
+            // T(HOME), D(LSFT), T(END), U(LSFT), D(LCTL), T(X), U(LCTL), END);
+                SEND_STRING(SS_TAP(X_HOME) SS_DOWN(X_LSFT) SS_TAP(X_END) SS_UP(X_LSFT) SS_LCTL("x"));
+                return false;
+            case MPASTELINE:
+            // T(HOME), D(LSFT), T(END), U(LSFT), D(LCTL), T(C), U(LCTL), END);
+                SEND_STRING(SS_TAP(X_END) SS_TAP(X_ENTER) SS_LCTL("v"));
+                return false;
+            case SEMICOLON_NEWLINE:
+                SEND_STRING(SS_TAP(X_END) ";" SS_TAP(X_ENTER));
+                return false;
+            case END_NEWLINE:
+                SEND_STRING(SS_TAP(X_END) SS_TAP(X_ENTER));
+                return false;
+            case DELL:
+                SEND_STRING("jim.dibb@dell.com");
+                return false;
+            case GMAIL:
                 SEND_STRING("jimdibb@gmail.com");
                 return false;
-            }
-		break;
-    case VS_EMAIL:
-            if (record->event.pressed) {
+            case VS_EMAIL:
                 SEND_STRING("jim.dibb@virtustream.com");
                 return false;
-            }
-		break;
-    case RS_AS:
-            if (record->event.pressed) {
+            case RS_AS:
                 autoshift_toggle();
                 layer_invert(RSMITH);
                 ergodox_right_led_1_off();
                 return false;
-            }
-		break;
-    case GAME_AS:
-            if (record->event.pressed) {
+            case GAME_AS:
                 autoshift_toggle();
                 layer_invert(GAME);
                 ergodox_right_led_1_off();
                 return false;
-            }
-		break;
-      }
-    return MACRO_NONE;
-};
+        }
+    }
+    return true;
+}
+
+// ! Any remaining macros below were not converted above as I just don't use them
+// const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+// {
+//   // MACRODOWN only works in this function
+//       switch(id) {
+//
+//         case MC_NEW_SEARCH_TAB:
+//              if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(T), T(K), U(LCTL), END);
+//             }
+//             break;
+//         case SCREEN_TAB_LEFT:
+//              if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(A), U(LCTL), T(P), END);
+//             }
+//             break;
+//         case SCREEN_TAB_RIGHT:
+//              if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(A), U(LCTL), T(N), END);
+//             }
+//             break;
+//         case SCREEN_NEW_TAB:
+//              if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(A), U(LCTL), T(C), END);
+//             }
+//         break;
+//         case SCREEN_COPY_MODE:
+//             if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(A), U(LCTL), T(ESC), END);
+//             }
+//         break;
+//         case SCREEN_PASTE:
+//             if (record->event.pressed) {
+//                 return MACRO( D(LCTL), T(A), U(LCTL), T(RBRC), END);
+//             }
+//         break;
+//         case SWITCH_NDS:
+//              if (record->event.pressed) {
+//                 return MACRO( D(LSFT), T(F11), U(LSFT), W(255), D(LALT), T(TAB), U(LALT), END);
+//             }
+//         break;
+//         case SHELL_RECALL_LAST_ARG_REMOVE_FIRST_COMMAND:
+//             if (record->event.pressed) {
+//                 return MACRO( T(UP), T(HOME), D(LALT), T(D), U(LALT), END);
+//             }
+//         break;
+
+//       }
+//     return MACRO_NONE;
+// };
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
