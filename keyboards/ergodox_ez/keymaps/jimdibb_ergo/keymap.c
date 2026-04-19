36084a63758de90e78e1d90d6b5c4c53a59eaae0
Jim Dibb <jimdibb@gmail.com>
2026-04-18 21:01:48 -0400

    mac changes

--- keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
+++ keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,                    KC_QUOT,     KC_COMM,    KC_DOT,     KC_P,        KC_Y,        MO(KEY_SEL),      KC_PGUP,    KC_F,        KC_G,        KC_C,          KC_R,         KC_L,         KC_SLSH,
     OSM(MOD_LGUI),              KC_A,       KC_O,       KC_E,       KC_U,        KC_I,                                      KC_D,        KC_H,        KC_T,          KC_N,         KC_S,         KC_MINS,
     OSM(MOD_LSFT),             KC_SCLN,     KC_Q,       KC_J,       KC_K,        KC_X,        MO(MAC_KEY_NAV),      KC_PGDN,    KC_B,        KC_M,        KC_W,          KC_V,         KC_Z,         MO(BRACKETS),
-    MO(MOUSE),                  KC_LWIN,    OSM(MOD_LALT),  MO(SYMBOL)  ,MO(NUMBER),                                        RSFT(RCTL(KC_TAB)), RCTL(KC_TAB), RCTL(KC_T), LALT(KC_LEFT), RCTL(KC_W),
+    MO(MOUSE),                  KC_LEFT_CTRL,    OSM(MOD_LALT),  MO(SYMBOL)  ,MO(NUMBER),                                        RSFT(RCTL(KC_TAB)), RCTL(KC_TAB), RCTL(KC_T), LALT(KC_LEFT), RCTL(KC_W),
                                        // thumb cluster                                                             // thumb cluster
                                                SFT_T(KC_PAUSE), LCTL(KC_S),                                         KC_LEFT,    KC_RIGHT,
                                                           RCTL(KC_DEL),                                             KC_UP,
@@ -357,7 +357,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MOUSE] = LAYOUT_ergodox(
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, //RGB_TOG, RGB_MOD
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU,
+       MAC_AS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU,
        KC_TRNS, KC_MUTE, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS,
        KC_SLEP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
        KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
@@ -506,17 +506,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             case RS_AS:
                 autoshift_toggle();
                 layer_invert(RSMITH);
-                ergodox_right_led_1_off();
+                //ergodox_right_led_1_off();
                 return false;
             case GAME_AS:
                 autoshift_toggle();
                 layer_invert(GAME);
-                ergodox_right_led_1_off();
+                //ergodox_right_led_1_off();
                 return false;
             case GAME_FNITE:
                 autoshift_toggle();
                 layer_invert(FORTNITE);
-                ergodox_right_led_1_off();
+                //ergodox_right_led_1_off();
                 return false;
         }
     }
@@ -594,14 +594,14 @@ void matrix_scan_user(void) {
 
     uint8_t layer = biton32(layer_state);
 
-    ergodox_board_led_off();
+    ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
         case NUMBER:
         case SYMBOL:
 		case BRACKETS:
-		//case SHELL_LAYER:
+		case MAC:
             ergodox_right_led_2_on();
             break;
         case KEY_NAV:
