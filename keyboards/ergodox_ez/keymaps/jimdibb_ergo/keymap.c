174416aeec70276d659ef852d7dd52f2bb6c45e3
Jim Dibb <jimdibb@gmail.com>
2026-04-30 21:38:56 -0400

    minus and = keys on number row

--- keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
+++ keyboards/ergodox_ez/keymaps/jimdibb_ergo/keymap.c
@@ -112,11 +112,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `----------------------'
  */
 [MAC] = LAYOUT_ergodox_pretty(
-    MT(MOD_LSFT, KC_ESC),       KC_1,      KC_2,      KC_3,      KC_4,       KC_5,       MEH_T(KC_6),     KC_7,      KC_8,       KC_9,       KC_0,        KC_F11,       KC_F12,       KC_BSLS,
-    KC_TAB,                    KC_QUOT,     KC_COMM,    KC_DOT,     KC_P,        KC_Y,        MO(KEY_SEL),      KC_PGUP,    KC_F,        KC_G,        KC_C,          KC_R,         KC_L,         KC_SLSH,
-    OSM(MOD_LGUI),              KC_A,       KC_O,       KC_E,       KC_U,        KC_I,                                      KC_D,        KC_H,        KC_T,          KC_N,         KC_S,         KC_MINS,
-    OSM(MOD_LSFT),             KC_SCLN,     KC_Q,       KC_J,       KC_K,        KC_X,        MO(MAC_KEY_NAV),      KC_PGDN,    KC_B,        KC_M,        KC_W,          KC_V,         KC_Z,         MO(BRACKETS),
-    MO(MOUSE),                  KC_LEFT_CTRL,    OSM(MOD_LALT),  MO(SYMBOL)  , M_CMD_SPACE,                                        RSFT(RCTL(KC_TAB)), RCTL(KC_TAB), LCMD(KC_LEFT), LCMD(KC_T),  LCMD(KC_W),
+    MT(MOD_LSFT,      KC_ESC),       KC_1,      KC_2,      KC_3,      KC_4,       KC_5,       MEH_T(KC_6),          KC_7,      KC_8,       KC_9,       KC_0,        KC_MINS,       KC_EQUAL,       KC_BSLS,
+    KC_TAB,           KC_QUOT,     KC_COMM,    KC_DOT,     KC_P,        KC_Y,               MO(KEY_SEL),            KC_PGUP,    KC_F,        KC_G,        KC_C,          KC_R,         KC_L,         KC_SLSH,
+    OSM(MOD_LGUI),    KC_A,       KC_O,       KC_E,       KC_U,        KC_I,                                                    KC_D,        KC_H,        KC_T,          KC_N,         KC_S,         KC_MINS,
+    OSM(MOD_LSFT),    KC_SCLN,     KC_Q,       KC_J,       KC_K,        KC_X,               MO(MAC_KEY_NAV),        KC_PGDN,    KC_B,        KC_M,        KC_W,          KC_V,         KC_Z,         MO(BRACKETS),
+    MO(MOUSE),        KC_LEFT_CTRL,    OSM(MOD_LALT),  MO(SYMBOL)  , M_CMD_SPACE,                                        RSFT(RCTL(KC_TAB)), RCTL(KC_TAB), LCMD(KC_LEFT), LCMD(KC_T),  LCMD(KC_W),
                                        // thumb cluster                                                             // thumb cluster
                                                RSFT(RCTL(KC_GRV)), LGUI(KC_S),                                         KC_LEFT,    KC_RIGHT,
                                                           RALT(KC_DEL),                                             KC_UP,
@@ -457,7 +457,7 @@ bool n_key_tapping_user(uint16_t keycode, keyrecord_t *record) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // 1. Handle keys that need BOTH press and release logic (like M_CMD_SPACE)
     switch(keycode) {
-        case M_CMD_SPACE:
+        case M_CMD_SPACE: // if held, toggle number mode, if tapped, start spotlight (CMD - SPACE)
             if (record->event.pressed) {
                     // Start a timer to see if it's a tap or hold
                     spc_tap_timer = timer_read();
